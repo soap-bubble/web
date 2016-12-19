668d612b392590c16100764c379609acb533d456
John Dean <jdean@shutterstock.com>
2016-12-19 13:03:26 -0600

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  target: "web",
+  target: 'web',
   entry: {
     app: './client/js/app',
-    vendor: ['lodash', 'three']
+    vendor: ['lodash', 'three'],
   },
   output: {
     path: path.join(__dirname, 'public/js'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: ['ng-annotate', 'babel-loader'],
+        loader: 'babel-loader',
       }, {
         test: /\.json$/,
-        loader: 'json'
-      }
-    ]
+        loader: 'json',
+      },
+    ],
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
-  ]
-}
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+  ],
+};

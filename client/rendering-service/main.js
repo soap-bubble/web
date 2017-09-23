e545b274778c61565faa6e350649449ecd227dd7
John Dean <john.holmes.dean@gmail.com>
2017-09-23 12:18:49 -0500

    typo

--- client/rendering-service/main.js
+++ client/rendering-service/main.js
@@ -90,7 +90,7 @@ export default function (parameters) {
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            ga('create', '${process.env.GOOGLE_ANALYTICS_ID || ''}'', 'auto');
+            ga('create', '${process.env.GOOGLE_ANALYTICS_ID || ''}', 'auto');
             ga('send', 'pageview');
           </script>
           <script>

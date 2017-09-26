f3e04f7e040439226ae1937e464223eeca15df61
John Dean <john.holmes.dean@gmail.com>
2017-09-25 22:03:37 -0500

    Removed dead code

--- client/js/morpheus/input/actions.js
+++ client/js/morpheus/input/actions.js
@@ -1,14 +1,11 @@
 import {
-  ADD_ONMOUSEUP,
-  ADD_ONMOUSEMOVE,
-  ADD_ONMOUSEDOWN,
-  ADD_ONTOUCHSTART,
-  ADD_ONTOUCHMOVE,
-  ADD_ONTOUCHEND,
-  ADD_ONTOUCHCANCEL,
   DISABLE_CONTROL,
   ENABLE_CONTROL,
 } from './actionTypes';
+
+export function keyPress(keyCode) {
+
+}
 
 export function disableControl() {
   return {
@@ -19,61 +16,5 @@ export function disableControl() {
 export function enableControl() {
   return {
     type: ENABLE_CONTROL,
-  };
-}
-
-export function addMouseUp(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONMOUSEUP,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addMouseMove(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONMOUSEMOVE,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addMouseDown(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONMOUSEDOWN,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addTouchStart(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONTOUCHSTART,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addTouchMove(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONTOUCHMOVE,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addTouchEnd(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONTOUCHEND,
-    payload: callback,
-    meta: { sceneOnly },
-  };
-}
-
-export function addTouchCancel(callback, sceneOnly = true) {
-  return {
-    type: ADD_ONTOUCHCANCEL,
-    payload: callback,
-    meta: { sceneOnly },
   };
 }
--- client/js/morpheus/input/reducer.js
+++ client/js/morpheus/input/reducer.js
@@ -1,63 +1,17 @@
 import createReducer from 'utils/createReducer';
 import {
-  ADD_ONMOUSEUP,
-  ADD_ONMOUSEMOVE,
-  ADD_ONMOUSEDOWN,
-  ADD_ONTOUCHSTART,
-  ADD_ONTOUCHMOVE,
-  ADD_ONTOUCHEND,
-  ADD_ONTOUCHCANCEL,
   DISABLE_CONTROL,
   ENABLE_CONTROL,
 } from './actionTypes';
 
-function reducerForType(type) {
-  return function reducer(ui, { payload: callback, meta }) {
-    let { [type]: handlerList } = ui;
-    let { removeOnSceneEnd } = ui;
-    const { sceneOnly } = meta;
-    if (sceneOnly) {
-      removeOnSceneEnd = Object.assign({}, removeOnSceneEnd);
-      let removeHandlerList = removeOnSceneEnd[type];
-      if (!removeHandlerList) {
-        removeHandlerList = [callback];
-      } else {
-        removeHandlerList = removeHandlerList.concat([callback]);
-      }
-      removeOnSceneEnd[type] = removeHandlerList;
-    }
-    handlerList = handlerList.concat([callback]);
-    const ret = {
-      ...ui,
-      [type]: handlerList,
-      removeOnSceneEnd,
-      enabled: true,
-    };
-    return ret;
-  };
-}
-
 const inputDefaults = {
-  onMouseUp: [],
-  onMouseMove: [],
-  onMouseDown: [],
-  onTouchStart: [],
-  onTouchMove: [],
-  onTouchEnd: [],
-  onTouchCancel: [],
+  enabled: true,
 };
 
 const reducer = createReducer(
   'input',
   inputDefaults,
   {
-    [ADD_ONMOUSEUP]: reducerForType('onMouseUp'),
-    [ADD_ONMOUSEMOVE]: reducerForType('onMouseMove'),
-    [ADD_ONMOUSEDOWN]: reducerForType('onMouseDown'),
-    [ADD_ONTOUCHSTART]: reducerForType('onTouchStart'),
-    [ADD_ONTOUCHMOVE]: reducerForType('onTouchMove'),
-    [ADD_ONTOUCHEND]: reducerForType('onTouchEnd'),
-    [ADD_ONTOUCHCANCEL]: reducerForType('onTouchCancel'),
     [DISABLE_CONTROL](state) {
       return {
         ...state,

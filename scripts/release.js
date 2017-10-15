49bdb983b358e7d6289ab1c4ac916917421c28ad
John Dean <john.holmes.dean@gmail.com>
2017-10-14 21:04:35 -0500

    Add release script

---
+++ scripts/release.js
@@ -0,0 +1,35 @@
+const { run, lernaExec } = require('./run')
+
+// check that we're not running through `yarn`,
+// which breaks because `npm whoami` returns nil
+try {
+  run(`npm whoami`, { useExec: true })
+} catch (err) {
+  console.warn('Cannot run as `yarn release`, must run as `npm run release`.')
+  process.exit()
+}
+
+// health check
+lernaExec(`git pull`)
+lernaExec(`npm run preversion`)
+
+// increment version numbers accross all package.json's
+run(`lerna publish --skip-npm --skip-git --exact`)
+const { version, packages } = require('../lerna.json')
+const versionStr = `v${version}`
+
+// commit & tag version bumps
+lernaExec(`git commit -am v${version}`)
+lernaExec(`git tag ${versionStr} -m ${versionStr}`)
+
+// push packages to npm & github
+lernaExec(`npm publish`)
+
+lernaExec(`git push && git push --tags`)
+
+// commit & push version bump at monorepo level
+run(`git add ${packages.join(' ')} lerna.json`)
+run(`git commit -m ${versionStr}`)
+run(`git tag ${versionStr} -m ${versionStr}`)
+run(`git push`)
+run(`git push --tags`)
---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const { spawnSync, execFileSync } = require('child_process')
+
+const run = (str, opts = {}) => {
+  console.log(str)
+  const [ cmd, ...args ] = str.split(' ')
+  if (opts.useExec) {
+    execFileSync(cmd, args, opts)
+  } else {
+    const { status } = spawnSync(cmd, args, Object.assign({}, opts, { stdio: 'inherit' }))
+    if (status !== 0) process.exit()
+  }
+}
+module.exports = run;
+module.exports.run = run;
+
+const lernaExec = (cmd, opts) => run(`lerna exec -- ${cmd}`, opts)
+
+module.exports.lernaExec = lernaExec;

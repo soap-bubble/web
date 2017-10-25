cfbc7ab114b3fc479abc59bae36fbbf885064c61
John Dean <john.holmes.dean@gmail.com>
2017-10-25 17:38:29 -0500

    Add bootstrap-social icons and style

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+const loaders = require('./index').loaders;
 module.exports = env => {
   const sassLoaders = ["css-loader", "postcss-loader", "sass-loader"];
   const cssLoaders = ["css-loader", "postcss-loader"];
@@ -8,9 +8,10 @@ module.exports = env => {
     entry: './build.js',
     output: {
       filename: 'dist.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     module: {
-      rules: [
+      rules: loaders.concat([
         {
           test: /\.(scss|sass)$/,
           use: env.development ? ["style-loader"].concat(sassLoaders) : ExtractTextPlugin.extract({ use: sassLoaders }),
@@ -19,7 +20,7 @@ module.exports = env => {
           test: /\.css$/,
           use:  env.development ? ["style-loader"].concat(cssLoaders) : ExtractTextPlugin.extract({ use: cssLoaders }),
         },
-      ],
+      ]),
     },
     plugins: [
       new ExtractTextPlugin({

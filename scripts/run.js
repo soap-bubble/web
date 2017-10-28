3f5a4882fc74f5ea1817ecff19d55ba957c1a90b
John Dean <john.holmes.dean@gmail.com>
2017-10-28 12:30:30 -0500

    udpated

--- scripts/run.js
+++ scripts/run.js
@@ -1,13 +1,12 @@
 const spawnSync = require('cross-spawn').sync;
 
-const run = (str, opts = {}, ignoreError = false) => {
+const run = (str, opts = {}) => {
   console.log(str)
   const [ cmd, ...args ] = str.split(' ')
   const { stdout, stderr, status } = spawnSync(cmd, args, Object.assign({}, opts, { encoding: 'utf8' }))
-  if (status !== 0 && !ignoreError) {
+  if (status !== 0) {
     console.log(stdout);
     console.error(stderr);
-    process.exit();
   }
   return stdout;
 }
@@ -16,6 +15,6 @@ module.exports.run = run;
 
 const lernaExec = (cmd, opts, ignoreError) => run(`lerna exec ${
   !ignoreError ? '--bail=false ' : ''
-}-- ${cmd}`, opts)
+}-- ${cmd}`, opts, ignoreError)
 
 module.exports.lernaExec = lernaExec;

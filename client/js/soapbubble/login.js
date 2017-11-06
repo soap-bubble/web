fe525b89da3568f9ce2bf737e5f6c5cb59df43c0
John Dean <john.holmes.dean@gmail.com>
2017-11-06 06:57:56 -0600

    Pass in auth config to login component
    Better development support

--- client/js/soapbubble/login.js
+++ client/js/soapbubble/login.js
@@ -1,9 +1,12 @@
+import { Observable } from 'rxjs';
+import axios from 'axios';
 import { Login } from '@soapbubble/components';
 import { install } from 'utils/createReducer';
 import createEpic from 'utils/createEpic';
 
 const login = new Login({
   rootSelector: state => state.login,
+  googleConfigProvider: () => axios.get(`${config.authHost}/google/oauth`).then(res => res.data),
 });
 
 install('login', login.reducer);

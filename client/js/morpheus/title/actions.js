6ffac21d0c42a3f9ed5a3ff259765dd5302426a1
John Dean <john.holmes.dean@gmail.com>
2018-04-07 11:33:30 -0500

    refactor

--- client/js/morpheus/title/actions.js
+++ client/js/morpheus/title/actions.js
@@ -1,24 +1,12 @@
 import {
-  Mesh,
-  OrthographicCamera,
-  PlaneGeometry,
   Scene,
-  ShaderMaterial,
-  TextureLoader,
 } from 'three';
-import {
-  Tween,
-  Easing,
-} from 'tween';
 import {
   createCamera,
   positionCamera,
   createRenderer,
 } from 'utils/three';
 import renderEvents from 'utils/render';
-import {
-  getAssetUrl,
-} from 'service/gamedb';
 import {
   actions as sceneActions,
 } from 'morpheus/scene';
@@ -28,6 +16,7 @@ import {
 import {
   titleDimensions,
 } from './selectors';
+import titleActionsFactory from './actions.title';
 import {
   DONE,
   MOUSE_CLICK,
@@ -35,131 +24,14 @@ import {
   START,
 } from './actionTypes';
 
-export const vertexShader = `
-uniform float time;
-varying vec2 vUv;
-void main()
-{
-  vUv = uv;
-  vec3 coord = position;
-  coord.z = coord.z + (sin(coord.y * 30.0 + coord.x * 30.0 + time) * 0.025);
-  vec4 mvPosition = modelViewMatrix * vec4( coord, 1.0 );
-
-  gl_Position = projectionMatrix * mvPosition;
-}
-`;
-
-export const titleFragmentShader = `
-varying vec2 vUv;
-uniform float time;
-uniform float amplitude;
-uniform float intensity;
-uniform float opacity;
-uniform sampler2D texture;
-
-void main() {
-  float scale = 1.8;
-  vec2 coord = (vUv.xy - 0.5) * scale + 0.5;
-  coord.x = coord.x + (sin(coord.y * intensity + time) * amplitude);
-  // coord.y = coord.y + (sin(coord.x * intensity / 25.0 + time) * amplitude);
-  vec4 mapTexel = texture2D( texture, coord.xy );
-  if (coord.x > 1.0
-    || coord.x < 0.0
-    || coord.y > 1.0
-    || coord.y < 0.0) {
-      gl_FragColor = vec4(0.0);
-    } else {
-      mapTexel.a *= opacity;
-      gl_FragColor = mapTexel;
-    }
-}
-`;
-
-function createGeometry() {
-  const geometry = new PlaneGeometry(1, 0.5, 64, 64);
-  return geometry;
-}
-
-function createMaterial({ uniforms }) {
-  const material = new ShaderMaterial({
-    uniforms,
-    vertexShader,
-    fragmentShader: titleFragmentShader,
-    transparent: true,
-  });
-  return material;
-}
-
-const textureLoader = new TextureLoader();
-
-function createTexture() {
-  const texture = textureLoader.load(getAssetUrl('GameDB/All/morpheus-title', 'png'));
-  return texture;
-}
-
-function createMesh({ material, geometry }) {
-  const mesh = new Mesh(
-    geometry,
-    material,
-  );
-  mesh.scale.set(3, 2, 1);
-  mesh.position.y = 0.5;
-  window.titleMesh = mesh;
-  return mesh;
-}
-
 function createScene({ object }) {
   const scene = new Scene();
   scene.add(object);
   return scene;
 }
 
-function startRenderLoop({ scene3D, camera, renderer, uniforms }) {
-  const v = {
-    get amplitude() {
-      return uniforms.amplitude.value;
-    },
-    set amplitude(value) {
-      uniforms.amplitude.value = value;
-    },
-    get intensity() {
-      return uniforms.intensity.value;
-    },
-    set intensity(value) {
-      uniforms.intensity.value = value;
-    },
-    get opacity() {
-      return uniforms.opacity.value;
-    },
-    set opacity(value) {
-      uniforms.opacity.value = value;
-    },
-  };
-  const opacityTween = new Tween(v)
-    .to({
-      opacity: 1,
-    }, 10000)
-    .easing(Easing.Sinusoidal.In)
-    .start();
-
-  // const intensityTween = new Tween(v)
-  //   .to({
-  //     intensity: 0,
-  //   }, 10000)
-  //   .easing(Easing.Exponential.In)
-  //   .start();
-
-  const amplitudeTween = new Tween(v)
-    .to({
-      amplitude: 0,
-    }, 10000)
-    .easing(Easing.Quintic.In)
-    .start();
-
-  const start = Date.now();
+function startRenderLoop({ scene3D, camera, renderer }) {
   const render = () => {
-    uniforms.time.value = (Date.now() - start) / 1000;
-
     renderer.render(scene3D, camera);
   };
   renderEvents.onRender(render);
@@ -171,37 +43,23 @@ function startRenderLoop({ scene3D, camera, renderer, uniforms }) {
 export function canvasCreated(canvas) {
   return (dispatch, getState) => {
     if (canvas) {
+      const titleActions = dispatch(titleActionsFactory());
       const { width, height } = titleDimensions(getState());
-      // const camera = new OrthographicCamera(-1, 1, 1, -1, 1, 2);
       const camera = createCamera({ width, height });
-      window.titleCamera = camera;
-      const uniforms = {
-        time: { type: 'f', value: 1.0 },
-        amplitude: { type: 'f', value: 0.25 },
-        intensity: { type: 'f', value: 15 },
-        opacity: { type: 'f', value: 0 },
-        texture: { type: 't', value: createTexture() },
-      };
       const renderer = createRenderer({ canvas, width, height });
       positionCamera({
         camera,
         vector3: { z: 2 },
       });
       const scene3D = createScene({
-        object: createMesh({
-          material: createMaterial({
-            uniforms,
-          }),
-          geometry: createGeometry(),
-        }),
+        object: titleActions.createObject3D(),
       });
-      window.titleScene = scene3D;
       startRenderLoop({
         scene3D,
         camera,
         renderer,
-        uniforms,
       });
+      titleActions.start();
       dispatch({
         type: SET_RENDER_ELEMENTS,
         payload: {

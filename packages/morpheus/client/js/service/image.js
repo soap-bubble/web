dc7e6496307dd47e1fe9bbbe4873416cf9a4a148
John Dean <jdean@shutterstock.com>
2018-07-17 17:33:15 -0400

    I think this fixes cross origin requests

--- packages/morpheus/client/js/service/image.js
+++ packages/morpheus/client/js/service/image.js
@@ -1,7 +1,7 @@
 export function loadAsImage(url = '') {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.crossorigin = 'anonymous';
+    img.crossOrigin = 'anonymous';
     img.src = url;
     img.onload = () => resolve(img);
     img.onerror = () => reject(new Error(`Failed to load image ${url}`));

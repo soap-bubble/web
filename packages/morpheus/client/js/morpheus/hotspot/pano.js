771eb70c294fe42d59e8c34d16ab3e735405e934
John Dean <john.holmes.dean@gmail.com>
2018-08-12 15:17:22 -0500

    Clean up event queueing

--- packages/morpheus/client/js/morpheus/hotspot/pano.js
+++ packages/morpheus/client/js/morpheus/hotspot/pano.js
@@ -26,8 +26,7 @@ import {
 } from 'morpheus/gamestate';
 import storeFactory from 'store';
 
-const mouseEventQueue = new Queue(1, 128);
-const actionQueue = new Queue(1, 12);
+const actionQueue = new Queue(1, 128);
 
 export default function ({
   dispatch,
@@ -55,116 +54,116 @@ export default function ({
       top: clientY - location.y,
       left: clientX - location.x,
     };
-    if (mouseEventQueue.getPendingLength()) {
-      dispatch(gameActions.setCursorLocation(currentSreenPosition));
-    }
-    mouseEventQueue.add(() => {
-      const state = store.getState();
-      const currentScene = sceneSelectors.currentSceneData(state);
-      if (currentScene === scene && !document.hidden) {
-        const canvas = castSelectors.forScene(scene).hotspot.canvas(state);
-        const hotspotScene3D = castSelectors.forScene(scene).hotspot.scene3D(state);
-        const panoScene3D = castSelectors.forScene(scene).pano.panoScene3D(state);
-        const camera = castSelectors.forScene(scene).hotspot.camera(state);
+    // if (mouseEventQueue.getPendingLength()) {
+    //   dispatch(gameActions.setCursorLocation(currentSreenPosition));
+    // }
+    const state = store.getState();
+    const currentScene = sceneSelectors.currentSceneData(state);
+    if (currentScene === scene && !document.hidden) {
+      const canvas = castSelectors.forScene(scene).hotspot.canvas(state);
+      const hotspotScene3D = castSelectors.forScene(scene).hotspot.scene3D(state);
+      const panoScene3D = castSelectors.forScene(scene).pano.panoScene3D(state);
+      const camera = castSelectors.forScene(scene).hotspot.camera(state);
 
-        // Convert mouse coordinates to x, y clamped between -1 and 1.  Also invert y
-        const y = (((canvas.height - currentSreenPosition.top) / canvas.height) * 2) - 1;
-        const x = (((currentSreenPosition.left - canvas.width) / canvas.width) * 2) + 1;
-        // Create a ray that travels from camera through screen at mouse location
-        raycaster.setFromCamera({ x, y }, camera);
-        // Got all faces that the ray intersects
-        const hotspotIntersects = raycaster.intersectObjects(hotspotScene3D.children, true);
-        // Map faces to hotspots...
-        const nowInHotspots = sortBy( // Sorted by cast index
-          uniq( // In the off chance that we hit both faces in a hotspot
-            hotspotIntersects.map(i => Math.floor(i.faceIndex / 2)),
-          ),
-        ) // Map back to hotspot index
-          .map(hotspotIndex => hotspots[hotspotIndex]);
+      // Convert mouse coordinates to x, y clamped between -1 and 1.  Also invert y
+      const y = (((canvas.height - currentSreenPosition.top) / canvas.height) * 2) - 1;
+      const x = (((currentSreenPosition.left - canvas.width) / canvas.width) * 2) + 1;
+      // Create a ray that travels from camera through screen at mouse location
+      raycaster.setFromCamera({ x, y }, camera);
+      // Got all faces that the ray intersects
+      const hotspotIntersects = raycaster.intersectObjects(hotspotScene3D.children, true);
+      // Map faces to hotspots...
+      const nowInHotspots = sortBy( // Sorted by cast index
+        uniq( // In the off chance that we hit both faces in a hotspot
+          hotspotIntersects.map(i => Math.floor(i.faceIndex / 2)),
+        ),
+      ) // Map back to hotspot index
+        .map(hotspotIndex => hotspots[hotspotIndex]);
 
-        const panoIntersects = raycaster.intersectObjects(panoScene3D.children[0].children.filter(c => c.name === 'pano'), true);
-        const currentPanoPosition = {};
-        const panoIntersect = panoIntersects.find((intersect) => {
-          if (intersect && intersect.uv) {
-            return true;
-          }
-          return false;
-        });
-        if (panoIntersect) {
-          const { uv, object: { material } } = panoIntersect;
-          material.map.transformUv(uv);
-          let left = uv.x * 2400;
-          let top = (uv.y * 1000) - 250;
-          if (uv.y > 0.5) {
-            left += 2400;
-            top = ((uv.y - 0.5) * 1000) - 250;
-          }
-          left -= 375;
-          if (left < 0) {
-            left += 3600;
-          }
-          currentPanoPosition.left = left;
-          currentPanoPosition.top = top;
+      const panoIntersects = raycaster.intersectObjects(panoScene3D.children[0].children.filter(c => c.name === 'pano'), true);
+      const currentPanoPosition = {};
+      const panoIntersect = panoIntersects.find((intersect) => {
+        if (intersect && intersect.uv) {
+          return true;
         }
-
-        const leavingHotspots = difference(wasInHotspots, nowInHotspots);
-        const enteringHotspots = difference(nowInHotspots, wasInHotspots);
-        const noInteractionHotspots = difference(hotspots, nowInHotspots);
-
-        // Update our state
-        if (wasMouseUpped) {
-          mouseDown = false;
+        return false;
+      });
+      if (panoIntersect) {
+        const { uv, object: { material } } = panoIntersect;
+        material.map.transformUv(uv);
+        let left = uv.x * 2400;
+        let top = (uv.y * 1000) - 250;
+        if (uv.y > 0.5) {
+          left += 2400;
+          top = ((uv.y - 0.5) * 1000) - 250;
         }
-
-        if (!mouseDown && wasMouseDowned) {
-          mouseDown = true;
-          startingPanoPosition = currentPanoPosition;
-          startingScreenPosition = currentSreenPosition;
-          lastMouseDown = Date.now();
+        left -= 375;
+        if (left < 0) {
+          left += 3600;
         }
-        const isMouseDown = mouseDown;
-
-        const interactionDistance = wasMouseUpped && Math.sqrt(
-          ((currentSreenPosition.left - startingScreenPosition.left) ** 2)
-          + ((currentSreenPosition.top - startingScreenPosition.top) ** 2),
-        );
-        const debounceDistance = isTouch ? 80 : 20;
-        const isClick = wasMouseUpped
-          && (Date.now() - lastMouseDown < 500) && interactionDistance < debounceDistance;
-
-        actionQueue.add(() => dispatch(handleEvent({
-          currentPosition: currentPanoPosition,
-          startingPosition: startingPanoPosition,
-          hotspots,
-          nowInHotspots,
-          leavingHotspots,
-          enteringHotspots,
-          noInteractionHotspots,
-          isClick,
-          isMouseDown,
-          wasMouseMoved,
-          wasMouseUpped,
-          wasMouseDowned,
-          handleHotspot: gamestateActions.handlePanoHotspot,
-        })));
-
-        wasInHotspots = nowInHotspots;
-        wasMouseMoved = false;
-        wasMouseUpped = false;
-        wasMouseDowned = false;
-
-        if (isTouch) {
-          lastTouchPosition = currentSreenPosition;
-        }
-
-        // Update cursor location and icon
-        actionQueue.add(() => dispatch(gameActions.setCursorLocation(currentSreenPosition)));
-
-        // Update the PanoAnim
-        return actionQueue.add(() => dispatch(castActionsForScene.update()));
+        currentPanoPosition.left = left;
+        currentPanoPosition.top = top;
       }
-      return null;
-    });
+
+      const leavingHotspots = difference(wasInHotspots, nowInHotspots);
+      const enteringHotspots = difference(nowInHotspots, wasInHotspots);
+      const noInteractionHotspots = difference(hotspots, nowInHotspots);
+
+      // Update our state
+      if (wasMouseUpped) {
+        mouseDown = false;
+      }
+
+      if (!mouseDown && wasMouseDowned) {
+        mouseDown = true;
+        startingPanoPosition = currentPanoPosition;
+        startingScreenPosition = currentSreenPosition;
+        lastMouseDown = Date.now();
+      }
+      const isMouseDown = mouseDown;
+
+      const interactionDistance = wasMouseUpped && Math.sqrt(
+        ((currentSreenPosition.left - startingScreenPosition.left) ** 2)
+        + ((currentSreenPosition.top - startingScreenPosition.top) ** 2),
+      );
+      const debounceDistance = isTouch ? 80 : 20;
+      const isClick = wasMouseUpped
+        && (Date.now() - lastMouseDown < 500) && interactionDistance < debounceDistance;
+
+      const eventOptions = {
+        currentPosition: currentPanoPosition,
+        startingPosition: startingPanoPosition,
+        hotspots,
+        nowInHotspots,
+        leavingHotspots,
+        enteringHotspots,
+        noInteractionHotspots,
+        isClick,
+        isMouseDown,
+        wasMouseMoved,
+        wasMouseUpped,
+        wasMouseDowned,
+        handleHotspot: gamestateActions.handlePanoHotspot,
+      };
+
+      wasInHotspots = nowInHotspots;
+      wasMouseMoved = false;
+      wasMouseUpped = false;
+      wasMouseDowned = false;
+
+      if (isTouch) {
+        lastTouchPosition = currentSreenPosition;
+      }
+
+      actionQueue.add(async () => {
+        // Update gamestate
+        await dispatch(handleEvent(eventOptions));
+        // Update cursor location and icon
+        await dispatch(gameActions.setCursorLocation(currentSreenPosition));
+        // Update the PanoAnim
+        await dispatch(castActionsForScene.update());
+      });
+    }
   }
 
   function rememberEvent(mouseEvent, isTouch) {
--- packages/morpheus/client/js/morpheus/hotspot/special.js
+++ packages/morpheus/client/js/morpheus/hotspot/special.js
@@ -28,8 +28,8 @@ import {
 } from 'utils/coordinates';
 
 const logger = loggerFactory('flatspot');
-const actionQueue = new Queue(1, 128);
-const cursorQueue = new Queue(1, 128);
+const mouseQueue = new Queue(1, 128);
+const actionQueue = new Queue(1, 12);
 
 export default function ({ dispatch, scene }) {
   const store = storeFactory();
@@ -48,101 +48,108 @@ export default function ({ dispatch, scene }) {
 
   function updateState({ clientX, clientY }, isTouchEnd) {
     const state = store.getState();
-    const inputEnabled = inputSelectors.enabled(state);
-    if (!inputEnabled) {
-      return;
+    const currentScene = sceneSelectors.currentSceneData(state);
+    if (currentScene === scene && !document.hidden) {
+      mouseQueue.add(() => {
+        const inputEnabled = inputSelectors.enabled(state);
+        if (!inputEnabled) {
+          return null;
+        }
+        const location = gameSelectors.location(state);
+        const hotspots = castSelectorForScene.special.hotspotData(state);
+        const isCurrent = sceneSelectors.currentSceneData(state) === scene;
+        const isExiting = castSelectorForScene.isExiting(state);
+        const acceptsMouseEvents = isCurrent && !isExiting;
+        if (!acceptsMouseEvents) {
+          return null;
+        }
+        const nowInHotspots = [];
+        const left = isTouchEnd ? lastTouchPosition.left : (clientX - location.x);
+        const top = isTouchEnd ? lastTouchPosition.top : (clientY - location.y);
+
+        const newWidth = gameSelectors.width(store.getState());
+        const newHeight = gameSelectors.height(store.getState());
+
+        const adjustedClickPos = screenToGame({
+          height: newHeight,
+          width: newWidth,
+          top,
+          left,
+        });
+
+        each(hotspots, (hotspot) => {
+          const {
+            rectTop,
+            rectBottom,
+            rectLeft,
+            rectRight,
+          } = hotspot;
+          if ((adjustedClickPos.top > rectTop
+            && adjustedClickPos.top < rectBottom
+            && adjustedClickPos.left > rectLeft
+            && adjustedClickPos.left < rectRight)
+          || (rectTop === 0
+            && rectLeft === 0
+            && rectRight === 0
+            && rectBottom === 0
+          )) {
+            nowInHotspots.push(hotspot);
+          }
+        });
+
+        const leavingHotspots = difference(wasInHotspots, nowInHotspots);
+        const enteringHotspots = difference(nowInHotspots, wasInHotspots);
+        const noInteractionHotspots = difference(hotspots, nowInHotspots);
+        const isClick = wasMouseUpped && Date.now() - lastMouseDown < 800;
+
+        if (wasMouseUpped) {
+          mouseDown = false;
+        }
+
+        if (!mouseDown && wasMouseDowned) {
+          mouseDown = true;
+          clickStartPos = adjustedClickPos;
+          lastMouseDown = Date.now();
+        }
+        const isMouseDown = mouseDown;
+        const eventOptions = {
+          currentPosition: adjustedClickPos,
+          startingPosition: clickStartPos,
+          hotspots,
+          nowInHotspots,
+          leavingHotspots,
+          enteringHotspots,
+          noInteractionHotspots,
+          isClick,
+          isMouseDown,
+          wasMouseMoved,
+          wasMouseUpped,
+          wasMouseDowned,
+          handleHotspot: gamestateActions.handleHotspot,
+        };
+
+        const promise = actionQueue.add(async () => {
+          await dispatch(handleEvent(eventOptions));
+          await dispatch(gameActions.setCursorLocation({ top, left }));
+        });
+
+        if (wasMouseUpped) {
+          clickStartPos = { top: -1, left: -1 };
+        }
+
+        wasInHotspots = nowInHotspots;
+        wasMouseMoved = false;
+        wasMouseUpped = false;
+        wasMouseDowned = false;
+        lastTouchPosition = {
+          top: clientX,
+          left: clientY,
+        };
+
+        return promise;
+      });
     }
-    const location = gameSelectors.location(state);
-    const hotspots = castSelectorForScene.special.hotspotData(state);
-    const isCurrent = sceneSelectors.currentSceneData(state) === scene;
-    const isExiting = castSelectorForScene.isExiting(state);
-    const acceptsMouseEvents = isCurrent && !isExiting;
-    if (!acceptsMouseEvents) {
-      return;
-    }
-    const nowInHotspots = [];
-    const left = isTouchEnd ? lastTouchPosition.left : (clientX - location.x);
-    const top = isTouchEnd ? lastTouchPosition.top : (clientY - location.y);
-
-    const newWidth = gameSelectors.width(store.getState());
-    const newHeight = gameSelectors.height(store.getState());
-
-    const adjustedClickPos = screenToGame({
-      height: newHeight,
-      width: newWidth,
-      top,
-      left,
-    });
-
-    each(hotspots, (hotspot) => {
-      const {
-        rectTop,
-        rectBottom,
-        rectLeft,
-        rectRight,
-      } = hotspot;
-      if ((adjustedClickPos.top > rectTop
-        && adjustedClickPos.top < rectBottom
-        && adjustedClickPos.left > rectLeft
-        && adjustedClickPos.left < rectRight)
-      || (rectTop === 0
-        && rectLeft === 0
-        && rectRight === 0
-        && rectBottom === 0
-      )) {
-        nowInHotspots.push(hotspot);
-      }
-    });
-
-    const leavingHotspots = difference(wasInHotspots, nowInHotspots);
-    const enteringHotspots = difference(nowInHotspots, wasInHotspots);
-    const noInteractionHotspots = difference(hotspots, nowInHotspots);
-    const isClick = wasMouseUpped && Date.now() - lastMouseDown < 800;
-
-    if (wasMouseUpped) {
-      mouseDown = false;
-    }
-
-    if (!mouseDown && wasMouseDowned) {
-      mouseDown = true;
-      clickStartPos = adjustedClickPos;
-      lastMouseDown = Date.now();
-    }
-    const isMouseDown = mouseDown;
-
-    cursorQueue.add(() => dispatch(gameActions.setCursorLocation({ top, left })), 'cursor location');
-    actionQueue.add(async () => {
-      await dispatch(handleEvent({
-        currentPosition: adjustedClickPos,
-        startingPosition: clickStartPos,
-        hotspots,
-        nowInHotspots,
-        leavingHotspots,
-        enteringHotspots,
-        noInteractionHotspots,
-        isClick,
-        isMouseDown,
-        wasMouseMoved,
-        wasMouseUpped,
-        wasMouseDowned,
-        handleHotspot: gamestateActions.handleHotspot,
-      }));
-      await dispatch(gameActions.setCursorLocation({ top, left }));
-    });
-
-
-    if (wasMouseUpped) {
-      clickStartPos = { top: -1, left: -1 };
-    }
-
-    wasInHotspots = nowInHotspots;
-    wasMouseMoved = false;
-    wasMouseUpped = false;
-    wasMouseDowned = false;
-    lastTouchPosition = {
-      top: clientX,
-      left: clientY,
-    };
+    return null;
   }
 
   function onMouseDown(mouseEvent) {

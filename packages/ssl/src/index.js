d5de5f2eafad8fedb819e501cd7813f3984ad2b8
John Dean <john.holmes.dean@gmail.com>
2019-02-17 16:22:17 -0600

    Starting to work on the routing side

---
+++ packages/ssl/src/index.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const config = require('config');
+const express = require('express');
+const builder = require('service-builder');
+
+const router = require('./router');
+
+const blueprint = builder({
+  app(router) {
+    const app = express();
+    app.use(router);
+    return app;
+  },
+  config,
+  router,
+  routes(config) {
+    return config.get('routes');
+  },
+  domains(config) {
+    return Object.values(config.routes).map(r => r.host);
+  },
+  email(config) {
+    return config.get('email');
+  },
+  isDebug(config) {
+    const val = config.get('debug');
+    if (val && val === 'false') {
+      return false;
+    }
+    return !!val;
+  },
+  server(isDebug) {
+    return isDebug ? 'https://acme-staging-v02.api.letsencrypt.org/directory' : 'https://acme-v02.api.letsencrypt.org/directory';
+  },
+  httpPort(config) {
+    return Number(config.get('httpPort'));
+  },
+  httpsPort(config) {
+    return Number(config.get('httpsPort'));
+  },
+  mongoUri(config) {
+    return config.get('mongoUri');
+  },
+  httpChallenge(isDebug) {
+    return require('le-challenge-standalone').create({
+      debug: isDebug,
+    });
+  },
+  greenlockOpts(app, store, email, httpChallenge, isDebug, server, domains) {
+    return {
+      app,
+      email,
+      server,
+      store,
+      debug: isDebug,
+      challenges: {
+        'http-01': httpChallenge,
+      },
+      configDir: path.resolve(__dirname, '.acme'),
+      agreeTos: true,
+      approvedDomains: domains,
+      communityMember: false,
+    };
+  },
+  greenlock(greenlockOpts) {
+    return require('greenlock-express').create(greenlockOpts);
+  },
+  store(mongoUri, isDebug) {
+    return require('le-store-mongodb').create({
+      url: mongoUri,
+      debug: isDebug,
+    });
+  },
+});
+
+
+const factory = blueprint.construct();
+
+factory.$((greenlock, httpPort, httpsPort) => {
+  console.log(`Listening on ${httpPort} and ${httpsPort}`);
+  greenlock.listen(httpPort, httpsPort);
+});
---
+++ packages/ssl/src/router.js
@@ -0,0 +1,31 @@
+const proxy = require('express-http-proxy');
+const { Router } = require('express');
+
+// Only accepts connections whose `Host` header matches the provided domains.
+function vhostDomainsRouter(configHost) {
+  const domains = Array.isArray(configHost) ? configHost : [configHost];
+  return (req, res, next) => {
+    if (domains.includes(req.headers.host)) {
+      next();
+    }
+  };
+}
+
+module.exports = function init(routes) {
+  const router = new Router();
+  for (let route of routes) {
+    const {
+      host,
+      route: hostRoute,
+      target,
+    } = route;
+    const middlewares = [];
+    if (host || host.length) {
+      middlewares.push(vhostDomainsRouter(host));
+    }
+    middlewares.push(proxy(target));
+
+    router.use(hostRoute, ...middlewares);
+  }
+  return router;
+}

e5d23f7bd49f14f69e1833d67d3f7b2123b1d8db
John Dean <john.holmes.dean@gmail.com>
2019-02-18 01:02:18 -0600

    v1.11.2

--- scripts/release.js
+++ scripts/release.js
@@ -1,27 +1,18 @@
-const { run, lernaExec } = require('./run')
-
-// health check
-lernaExec(`git pull`)
-// lernaExec(`npm run preversion`)
-
+const argv = require('minimist')(process.argv.slice(2));
+const { run, lernaExec } = require('./run');
 const updated = JSON.parse(run('lerna updated --json'));
 
+const bump = argv.bump ? argv.bump : 'patch';
+
 // increment version numbers accross all package.json's
-run(`lerna publish --skip-npm --skip-git --exact`, { stdio: 'inherit' });
+run(`lerna publish --skip-npm --skip-git --exact ${bump}`);
 const { version, packages } = require('../lerna.json')
 const versionStr = `v${version}`
-
-// commit & tag version bumps
-lernaExec(`git commit -am v${version}`, null, true)
-lernaExec(`git tag ${versionStr} -m ${versionStr}`)
 
 // push packages to npm & github (but only non-private repos)
 updated
   .filter(update => !update.private)
   .forEach(update => run(`lerna exec --scope ${update.name} -- npm publish --access=public`));
-
-lernaExec(`git push`)
-lernaExec(`git push --tags`)
 
 // commit & push version bump at monorepo level
 run(`git add ${packages.join(' ')} lerna.json`)

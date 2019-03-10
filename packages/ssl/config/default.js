2ccd9f4f5f04c70a07168cc9e0fed9d641b3c456
John Dean <john.holmes.dean@gmail.com>
2019-03-09 23:17:23 -0800

    reorder

--- packages/ssl/config/default.js
+++ packages/ssl/config/default.js
@@ -5,27 +5,31 @@ module.exports = {
   mongoUri: 'mongodb://mongo-cluster/greenlock?replicaSet=rs0',
   routes: [
     {
+      host: 'soapbubble.online',
+      route: '/auth',
+      target: 'http://auth:4000',
+    },
+    {
+      host: 'soapbubble.online',
+      route: '/morpheus',
+      target: 'http://morpheus:8050',
+    },
+    {
       host: 'morpheus.soapbubble.online',
       route: '/',
       redirect: 'https://soapbubble.online/morpheus',
     },
     {
       host: 'soapbubble.online',
-      route: '/auth',
-      target: 'http://auth:4000',
-    }, {
-      host: 'soapbubble.online',
-      route: '/morpheus',
-      target: 'http://morpheus:8050',
-    }, {
-      host: 'soapbubble.online',
       route: '/',
       target: 'http://core:8060',
-    }, {
+    },
+    {
       host: 'texascoc.net',
       route: '/',
       target: 'http://texascoc:8060',
-    }, {
+    },
+    {
       host: 'johndeanresume.com',
       route: '/',
       target: 'http://branes:5000',

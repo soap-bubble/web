60c1ed84862696904a86d7851c8760ec679d8b05
John Dean <john.holmes.dean@gmail.com>
2019-06-21 23:47:45 -0700

    close ws socket errors

--- packages/ssl/src/proxy/index.js
+++ packages/ssl/src/proxy/index.js
@@ -2,9 +2,9 @@ const httpProxy = require('http-proxy');
 
 module.exports = function init(rulesMap) {
   const proxy = httpProxy.createServer({ ws: true, changeOrigin: true });
-  proxy.on('error', (err, req, res) => {
-    res.writeHead(502, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: 'not responding' }));
+  proxy.on('error', (err, req, socket) => {
+    socket.send(JSON.stringify({ error: 'not responding' }));
+    socket.close();
   });
   return proxy;
 }

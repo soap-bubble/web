704e4ee1d7c65b0e621bb91ff2d3184f78226e75
CaptEmulation <john.holmes.dean@gmail.com>
2020-04-24 14:45:46 -0500

    Try out rancher pipeline

---
+++ packages/bot/config/custom-environment-variables.js
@@ -0,0 +1,14 @@
+module.exports = {
+  origins: 'BOT_ORIGIN',
+  profileId: 'BOT_PROFILE_ID',
+  discord: {
+    token: 'BOT_DISCORD_TOKEN',
+  },
+  twitch: {
+    clientID: 'BOT_TWITCH_CLIENT_ID',
+    clientSecret: 'BOT_TWITCH_CLIENT_SECRET',
+    webhook: 'BOT_TWITCH_WEBHOOK',
+    secret: 'BOT_TWITCH_SECRET',
+  },
+  port: 'BOT_PORT',
+}
